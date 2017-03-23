825fe00b0eed857f35766d0322baf766205665d8
gtmorais <gtmorais@gmail.com>
2017-03-23 00:33:26 -0400

    Delete note

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
 import { AngularFire, FirebaseListObservable } from 'angularfire2'
 
 @Component({
@@ -9,8 +8,79 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2'
 })
 export class HomePage {
   notes: FirebaseListObservable<any>
-  constructor(public navCtrl: NavController, public af: AngularFire) {
+  constructor(public navCtrl: NavController, public af: AngularFire,
+    public alertCtrl: AlertController, 
+    public actionSheetCtrl: ActionSheetController
+  ) {
     this.notes = af.database.list('/notes')
+
+
   }
 
+   showOptions(itemId, value){
+    let actionSheet = this.actionSheetCtrl.create(
+      {
+        title:"What do you want to do?",
+        buttons:[
+          {
+            text:"Delete",
+            role:"destructive",
+            handler:()=>{
+              this.removeItem(itemId);
+            }
+          },
+          {
+            text:"Update",
+            handler:()=>{
+              this.updateItem(itemId, value);
+            }
+          },
+          {
+            text:"Cancel",
+            role: "cancel",
+            handler:()=>{
+              console.log("Cancel");
+            }
+          }
+        ]
+      }
+    );
+    actionSheet.present();
+  }
+
+
+  removeItem(itemId: string){
+    console.log(itemId);
+    this.notes.remove(itemId);
+  }
+
+  updateItem(item, v){
+    console.log(item, v);
+    let prompt = this.alertCtrl.create({
+      title:"Update value",
+      message:"Update value for the item",
+      inputs:[
+        {
+          name:'value',
+          placeholder:"new value",
+          value:v
+        }
+      ],
+      buttons:[
+        {
+          text:'Cancel',
+          handler:data=>{
+            console.log("Cancel update");
+          }
+        },{
+          text:'Save',
+          handler:data=>{
+            console.log(data);
+            this.notes.update(item, data);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
